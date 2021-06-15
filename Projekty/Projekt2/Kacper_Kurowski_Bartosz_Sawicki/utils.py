3b310601de9abc688e448b38a445fd59ca536083
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-06-15 14:05:47 +0200

    update utils

--- Projekty/Projekt2/Kacper_Kurowski_Bartosz_Sawicki/utils.py
+++ Projekty/Projekt2/Kacper_Kurowski_Bartosz_Sawicki/utils.py
@@ -2,6 +2,10 @@ import os
 import pandas as pd
 import numpy as np
 
+from itertools import combinations, permutations
+from scipy.special import kl_div
+
+
 def get_colnames(dataset_path):
 	colnames = pd.read_csv(os.path.join(dataset_path,'features.txt'),
 		delim_whitespace=True, header=None, index_col=0)
@@ -54,3 +58,54 @@ def get_data_train(dataset_path='UCI HAR Dataset'):
 	subject = get_subject(dataset_path)
 	df = pd.concat([X, activity, subject], axis=1)
 	return df
+
+def get_columns(df:pd.DataFrame, n_cols:int=20, method:str='chosen', n_bins:int=5):
+	if method not in ('chosen', 'avg'):
+		raise ValueError("method parameter should be one of ('chosen', 'avg')")
+
+	activities = np.unique(df['activity'])
+	activities_dict = {activity: None for activity in activities}
+
+	for activity in activities_dict.keys():
+		numerical_features = df[df['activity']==activity].drop(['activity', 'subject'], axis='columns')
+		activities_dict[activity] = numerical_features.apply(
+			lambda column: 
+			np.histogram(column, bins=n_bins, density=True, range=(-1,1))[0])
+	
+	combi_colnames = list(map('-'.join, list(permutations(activities,2))))
+	combi_df = pd.DataFrame(columns = combi_colnames)
+
+	numerical_features = df.drop(['activity', 'subject'], axis='columns').columns
+	for feature in numerical_features:
+		combi_df = combi_df.append(pd.Series(np.repeat(feature, 30), name=feature, index=combi_colnames))
+
+	def replace_inf_and_sum(feature1, feature2, x):
+		kl = kl_div(
+			activities_dict[feature1][x],
+			activities_dict[feature2][x])
+		return sum(map(lambda x: 100 if x>100 else x, kl))
+
+	def count_inf(feature1, feature2, x):
+		return sum(
+			np.isinf(          # tu zliczamy infy
+				kl_div(
+					activities_dict[feature1][x],
+					activities_dict[feature2][x])))
+
+	def take_median(feature1, feature2, x):
+		return np.median(          
+				kl_div(
+					activities_dict[feature1][x],
+					activities_dict[feature2][x]))
+	
+	def apply_to_column(column):
+		feature1, feature2 = column.name.split('-')
+		return column.apply(lambda x: replace_inf_and_sum(feature1, feature2, x)) 
+		# w tej lambdzie można wybrać inną funkcję
+
+	result = combi_df.apply(apply_to_column)
+	if method == 'chosen':
+		return result.apply(lambda x: max(x), axis=1).sort_values(ascending=False).head(n_cols).index.to_list()
+	if method == 'avg':
+		return result.apply(lambda x: sum(x), axis=1).sort_values(ascending=False).head(n_cols).index.to_list()
+
